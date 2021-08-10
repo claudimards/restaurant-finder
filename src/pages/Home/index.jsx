d5f1d853b7fabb861a70b606df7e7bdedd047eab
Claudimar <claudimards@gmail.com>
2021-08-10 20:12:54 -0300

    Carousel finalizado

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import TextField, { Input } from '@material/react-text-field';
+import MaterialIcon from '@material/react-material-icon';
 
-import { Container } from './styles';
+import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import { Card } from '../../components';
 
-const Home = () => <Container>Hello World</Container>;
+import { Wrapper, Container, Carousel, Search, Logo, Map, CarouselTitle } from './styles';
+
+const Home = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const settings = {
+    dots: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScrool: 1,
+    adaptiveHeight: true,
+  };
+
+  return (
+    <Wrapper>
+      <Container>
+        <Search>
+          <Logo src={logo} alt="Logo" />
+          <TextField
+            label="Pesquisar Restaurantes"
+            outlined
+            trailingIcon={<MaterialIcon role="button" icon="search" />}>
+            <Input value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+          </TextField>
+          <CarouselTitle>Na sua Área</CarouselTitle>
+          <Carousel {...settings}>
+            <Card photo={restaurante} title="nome" />
+            <Card photo={restaurante} title="nome" />
+            <Card photo={restaurante} title="nome" />
+            <Card photo={restaurante} title="nome" />
+            <Card photo={restaurante} title="nome" />
+            <Card photo={restaurante} title="nome" />
+            <Card photo={restaurante} title="nome" />
+          </Carousel>
+        </Search>
+      </Container>
+      <Map />
+    </Wrapper>
+  );
+};
 
 export default Home;
